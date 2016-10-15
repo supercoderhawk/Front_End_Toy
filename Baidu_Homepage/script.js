e11453024f6a189efb7695bdd5c7e003460afa96
SuperCoderX <xyb930826@gmail.com>
2016-10-15 17:51:12 +0800

    some changes

--- Baidu_Homepage/script.js
+++ Baidu_Homepage/script.js
@@ -10,10 +10,29 @@ function hideSearchMenu(){
     menu.style.display = 'none';
 }
 
+//显示侧边栏
+function displaySidebar(){
+    sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'block';
+}
+
+//隐藏侧边栏
+function hideSidebar(){
+    sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'none';
+
+}
+
 window.onload = function(){
     //绑定设置按钮的鼠标事件
     setting = document.getElementById('setting');
     setting.onmousemove = displaySearchMenu;
     setting.onmouseout = hideSearchMenu;
-    
+    //绑定更多功能的鼠标事件
+    sidebarTitle = document.getElementById('sidebarTitle');
+    sidebar = document.getElementById('sidebar');
+    sidebarTitle.onmousemove = displaySidebar;
+    sidebar.onmouseleave = hideSidebar;
+
+
 }
